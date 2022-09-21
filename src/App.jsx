4ea63d8f4ad1bac1f7f114f07cb4c2eb7953eb76
prajaktahadole <prajaktahadole@gmail.com>
2022-09-21 18:50:24 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Myselect from './Select'
+import React from 'react';
+
+const options=[
+  {label:'Red',value:'red'},
+  {label:'Blue',value:'blue'},
+  {label:'Green',value:'green'},
+  {label:'Yellow',value:'yellow'},
+  {label:'black',value:'black'},
+  {label:'tail',value:'tail'},
+  {label:'brown',value:'brown'},
+  {label:'white',value:'white'},
+]
+function onChangeInput(value){
+  console.log(value);
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+       <Myselect isMulti={true} className="tick" defaultValue={[options[3]]} onChange={onChangeInput} options={options} label="Choose Your Favourite Colour" />
     </div>
   )
 }
---
+++ src/Select.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Select from 'react-select';
+
+function Myselect({style,label,options,onChange,defaultValue,isMulti}){
+
+    return <div style={style}>
+        <h1>{label}</h1>
+        <Select  isMulti={isMulti} options={options} onChange={onChange} defaultValue={defaultValue}/>
+    </div>
+}
+
+export default Myselect;
